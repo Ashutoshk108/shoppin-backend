1f83ef0640d9503402b538f8d15082c684b9f699
ashutoshk108 <ashutosh2135@gmail.com>
2025-01-11 16:33:04 +0530

    prebuild update

--- prebuild.sh
+++ prebuild.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
+# Update package lists
+sudo apt-get update
+pip install uvicorn
+
+# Install missing Playwright dependencies
+sudo apt-get install -y \
+    libgstreamer-gl1.0-0 \
+    libgstreamer-plugins-base1.0-0 \
+    libavif15 \
+    libenchant-2-2 \
+    libsecret-1-0 \
+    libmanette-0.2-0 \
+    libgles2
+
+# Install Python dependencies
 pip install -r requirements.txt
+
+# Install Playwright browsers
 playwright install
